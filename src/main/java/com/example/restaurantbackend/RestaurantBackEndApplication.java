fd274b5ab4c8c2a8f752b184fe175d556572fa16
Fiteira <leandrofiteira@hotmail.com>
2023-07-20 13:30:23 +0100

    build: building the project and add the docker-compose

---
+++ src/main/java/com/example/restaurantbackend/RestaurantBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restaurantbackend/RestaurantBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
